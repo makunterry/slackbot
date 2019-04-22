8e555661e4a63e6314c032a060f64d0d857958a7
Kun Ma <kun.ma@citrix.com>
2019-04-22 21:38:27 +0800

    test
    Signed-off-by: Kun Ma <kun.ma@citrix.com>

--- api/controllers/Controller.js
+++ api/controllers/Controller.js
@@ -1,22 +1,24 @@
 'use strict';
 
 var fs = require('fs');
+var util = require('util');
 var logfile0 = './command.log';
 var logfile1 = './botcallback.log';
 
-function reqlog(filename, req) {
+function reqlog(filename, req, title) {
+    fs.writeFileSync(filename,util.format('*************************** Request to %s ***************************\n', title), {flag:'a+'});
     fs.writeFileSync(filename,'===================== params ============================\n', {flag:'a+'});
     fs.writeFileSync(filename, JSON.stringify(req.params), {flag:'a+'});
     fs.writeFileSync(filename,'\n===================== query ============================\n', {flag:'a+'});
     fs.writeFileSync(filename, JSON.stringify(req.query), {flag:'a+'});
     fs.writeFileSync(filename,'\n===================== body ============================\n', {flag:'a+'});
     fs.writeFileSync(filename, JSON.stringify(req.body), {flag:'a+'});
-    fs.writeFileSync(filename,"\n===================== ***** ============================\n\n", {flag:'a+'});
+    fs.writeFileSync(filename,"\n******************************************************\n\n", {flag:'a+'});
 };
 
 exports.command = function(req, res) {
-    reqlog(logfile0, req);
-    res.json(req.body);
+    reqlog(logfile0, req, 'command');
+    res.sendStatus(200);
 };
 
 exports.welcome = function(req, res) {
@@ -24,7 +26,6 @@ exports.welcome = function(req, res) {
 };
 
 exports.botcallback = function(req, res) {
-    reqlog(logfile1, req);
+    reqlog(logfile1, req, 'botcallback');
     res.send(req.body.challenge);
-    //res.json(req.body);
 };
