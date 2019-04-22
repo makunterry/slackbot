8bda2fcdb5feedee5c1db060d8e79876564d0322
Kun Ma <kun.ma@citrix.com>
2019-04-22 16:32:49 +0800

    test
    Signed-off-by: Kun Ma <kun.ma@citrix.com>

--- api/controllers/Controller.js
+++ api/controllers/Controller.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 var logfile = './console.log';
 
 exports.command = function(req, res) {
-    fs.writeFileSync(logfile,'===================== params ============================', {flag:'a+'});
-    fs.writeFileSync(logfile, req.params, {flag:'a+'});
-    fs.writeFileSync(logfile,'===================== query ============================', {flag:'a+'});
-    fs.writeFileSync(logfile, req.query, {flag:'a+'});
+    fs.writeFileSync(logfile,'===================== params ============================\n', {flag:'a+'});
+    fs.writeFileSync(logfile, JSON.stringify(req.params), {flag:'a+'});
+    fs.writeFileSync(logfile,'===================== query ============================\n', {flag:'a+'});
+    fs.writeFileSync(logfile, JSON.stringify(req.query), {flag:'a+'});
     fs.writeFileSync(logfile,"===================== ***** ============================\n\n", {flag:'a+'});
     res.sendStatus(200);
 };
